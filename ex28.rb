3adafc7173af4c8dba3e4105a5a2910381759cfe
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-05-27 21:14:15 +0200

    Finish up to ex.29

---
+++ ex28.rb
@@ -0,0 +1,16 @@
+true && true
+false && true
+1 == 1 && 2 == 1
+"test" == "test"
+1 == 1 || 2 != 1
+true && 1 == 1
+false && 0 != 0
+true || 1 == 0
+"test" == "testing"
+!(true && false)
+!(1 == 1 && 0 != 1)
+!(10 == 1 || 1000 == 1000)
+!("testing" == "testing" && "Zed" == "Cool Guy")
+1 == 1 && (!("testing" == 1 || 1 == 0))
+"chunky" == "bacon" && (!(3 == 4 || 3 == 3))
+3 == 3 && (!("testing" == "testing" || "Ruby" == "Fun"))
---
+++ ex29.rb
@@ -0,0 +1,14 @@
+people = 100
+cats = 130
+dogs = 60
+
+if people < cats || cats > dogs
+	puts "Too many cats!"
+end
+
+dogs += 5
+
+if people >= dogs
+	"People are greater than or equal to dogs"
+end
+
