c378b2720981acee39ffe0128b06495e0d25b258
Sandra Adams-Hallie <sandra.adamshallie@gmail.com>
2016-05-28 12:48:23 +0200

    Finish up to ex. 32

---
+++ ex32.rb
@@ -0,0 +1,32 @@
+the_count = [1, 2, 3, 4, 5]
+fruits = ['apples', 'oranges', 'pears', 'apricots']
+change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
+
+# this first kind of for-loop goes through a list
+# in a more traditional style found in other languages
+the_count.each do |number|
+	puts "This is count #{number}"
+end
+
+# same as above, but in a more Ruby style
+# this and the next one are the preferred 
+# way Ruby for-loops are written
+fruits.each do |fruit|
+	puts "A fruit of type: #{fruit}"
+end
+
+# also we can go through mixed lists too
+# note this is yet another style, exactly like above
+# but a different syntax (way to write it).
+change.each {|i| puts "I got #{i}" }
+
+elements = []
+
+(0..5).each do |i|
+	puts "adding #{i} to the list"
+	elements << i #same as elements.push(i)
+end
+
+elements.each {|i| puts "Element was #{i}"}
+
+
